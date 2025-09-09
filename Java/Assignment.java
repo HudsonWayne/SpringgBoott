ac5f994ca3eae5f780b95b50807dab34b1083d7d
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-09 14:12:55 +0200

    initial commit

---
+++ Java/Assignment.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int num = 30;
+    num *= 2;
+    num += 5;
+    System.out.println(num);
+    }
+    
+}
