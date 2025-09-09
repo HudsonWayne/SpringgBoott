53f44523fb015d5d39231895fcde59ac1d154ed0
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-09 14:24:55 +0200

    initial commit

--- Java/Assignment.java
+++ Java/Assignment.java
@@ -6,6 +6,34 @@ public class Assignment {
     num *= 2;
     num += 5;
     System.out.println(num);
+
+
+
+
+    //Excersise 
+    int x = 12;
+    int y =  5;
+
+    //Arithmetic 
+    System.out.println("x + y =" + (x + y));
+    System.out.println("x - y=" + (x -y));
+    System.out.println("x + y =" + (x + y));
+    System.out.println("x - y=" + (x * y));
+    System.out.println("x + y =" + (x / y));
+    System.out.println("x - y=" + (x % y));
+
+
+
+    //comparison
+
+    System.out.println("Is X greater than y?" + (x > y));
+    System.out.println(" is x equal to " + (x==y));
+
+    //Logical
+    boolean condition1 =(x > y);
+    boolean condition2 = (y> 10);
+
+
     }
     
 }
