00b1bf4185418ee251156c4798ff660f32782b68
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-09 13:51:47 +0200

    initial commit

---
+++ Java/Operators.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));  
+        System.out.println("a % b = " + (a % b));   
+        
+    }
+}
