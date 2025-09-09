05f85fecee57b733dcffc827476b115b8befd6bb
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-09 11:29:35 +0200

    initial commit

--- Java/Variable.java
+++ Java/Variable.java
@@ -2,7 +2,11 @@ package Java;
 
 public class Variable {
     public static void main(String[] args) {
-        
+        int age = 20;
+        double price = 19.99;
+        boolean isJavaFun = true;
+        char grade = 'A';
+        String name = "Wayne";
     }
     
 }
