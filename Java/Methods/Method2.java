2b60e52e664e36cf5e366a78fcd29fc3f9659037
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-15 09:42:13 +0200

    initial commit

--- Java/Methods/Method2.java
+++ Java/Methods/Method2.java
@@ -10,14 +10,3 @@ public class Method2 {
 }
 
 
-public class  Method {
-    static void greet2(String name){
-        System.out.println("Hello," + name + "!");
-    }
-
-    public static void main(String[] args) {
-        greet2("Wayne");
-        greet2("Tanaka")
-    }
-    
-}
---
+++ Java/Methods/MethodValue.java
@@ -0,0 +1,12 @@
+package Java.Methods;
+
+public class MethodValue {
+    static int add(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = add(5,7);
+        System.out.println("The sum is: " + result);
+    }
+    
+}
