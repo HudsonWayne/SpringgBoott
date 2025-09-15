5ca003bfcf1cf0fc7d8709c750f1902132c88e06
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-15 14:11:40 +0200

    initial commit

---
+++ Java/JCF/ArryList.java
@@ -0,0 +1,13 @@
+package Java.JCF;
+import java.util.ArrayList;
+
+public class ArryList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Wayne");
+        names.add("Tanaka");
+        names.add("Hudson");
+    }
+    
+}
