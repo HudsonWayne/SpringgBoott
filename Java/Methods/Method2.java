13a08bdbbfd8ed7e595714283bfb53b039d10244
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-15 09:30:16 +0200

    initial commit

---
+++ Java/Methods/Method2.java
@@ -0,0 +1,10 @@
+package Java.Methods;
+
+public class Method2 {
+    static void sayHello(){
+        System.out.println("Hello, im new here!");
+    }
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
