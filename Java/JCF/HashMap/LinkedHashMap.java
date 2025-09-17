8ac1ff514e44fafb217f47b256c6aaa9b85a38f2
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-17 14:19:38 +0200

    initial commit

---
+++ Java/JCF/HashMap/LinkedHashMap.java
@@ -0,0 +1,9 @@
+package Java.JCF.HashMap;
+import java.util.LinkedHashMap;
+
+public class LinkedHashMap {
+    public static void main(String[] args) {
+        LinkedHashMap<String, String> capitals = new LinkedHashMap();
+        capitals.put("Zimbabwe", "Harare");
+    }
+}
