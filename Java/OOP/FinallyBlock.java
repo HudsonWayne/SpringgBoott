e227af01e311e56a637312d90f29a66b734d4aa5
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-24 10:35:42 +0200

    initial commit

---
+++ Java/OOP/FinallyBlock.java
@@ -0,0 +1,12 @@
+package Java.OOP;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
