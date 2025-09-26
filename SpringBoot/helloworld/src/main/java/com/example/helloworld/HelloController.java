f8c981990e32af8c6e3a0cb5a66b9589fbd158bd
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-26 11:24:05 +0200

    initial commit

--- SpringBoot/helloworld/src/main/java/com/example/helloworld/HelloController.java
+++ SpringBoot/helloworld/src/main/java/com/example/helloworld/HelloController.java
@@ -1,14 +1,20 @@
-package SpringBoot.helloworld.src.main.java.com.example.helloworld;
+package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringApplication;
-
-import com.example.helloworld.HelloworldApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+public class HelloWorldApplication {
 
-public class HelloController {
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class,args);
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
     }
 }
