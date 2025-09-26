38b77e3ac48d2e57972974c0a96145d06acd0375
HudsonWayne <waynewaynebenhura@gmail.com>
2025-09-26 11:19:22 +0200

    initial commit

--- SpringBoot/helloworld/src/main/java/com/example/helloworld/HelloController.java
+++ SpringBoot/helloworld/src/main/java/com/example/helloworld/HelloController.java
@@ -1,5 +1,14 @@
 package SpringBoot.helloworld.src.main.java.com.example.helloworld;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringApplication;
+
+import com.example.helloworld.HelloworldApplication;
+
+@SpringBootApplication
+
 public class HelloController {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class,args);
+    }
 }
